0e776500baaf022be972c4e5938a440640c4fdf4
Nagy Máté <nagy.mano.mate@gmail.com>
2024-11-21 20:34:38 +0100

    argsdrfsef

--- IKT-2025-01-08/Kreta/Kreta.ConsoleApp/GlobalImports.cs
+++
@@ -1,9 +0,0 @@
-﻿global using System;
-global using System.Collections;
-global using System.Collections.Generic;
-global using System.ComponentModel.DataAnnotations;
-global using System.ComponentModel.DataAnnotations.Schema;
-global using System.Linq;
-global using System.Text;
-global using System.Threading.Tasks;
-global using Microsoft.EntityFrameworkCore;

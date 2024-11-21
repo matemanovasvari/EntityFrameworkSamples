80fd3bf980325092e22c8e195e1e4a93312571bf
Nagy Máté <nagy.mano.mate@gmail.com>
2024-11-21 20:33:47 +0100

    AddStudent-ig kész

---
+++ IKT-2025-01-08/Kreta/Kreta.ConsoleApp/OperationFunctions.cs
@@ -0,0 +1,32 @@
+﻿using Kreta.Database.Entities;
+using Kreta.Database;
+
+namespace Kreta.ConsoleApp;
+
+public class OperationFunctions
+{
+    public static async Task AddStudent(ApplicationDbContext context)
+    {
+        Console.Clear();
+        Console.WriteLine("---------Add new student---------\n");
+
+        var student = new StudentEntity
+        {
+            EducationalCode = GetFunctions.GetEducationCode(),
+            Name = GetFunctions.GetStudentName(),
+            DateOfBirth = GetFunctions.GetStudentDateOfBirth(),
+            MothersName = GetFunctions.GetMothersName(),
+            Address = new AddressEntity
+            {
+                CountryName = GetFunctions.GetCountryName(),
+                CityName = GetFunctions.GetCityName(),
+                PostalCode = GetFunctions.GetPostalCode(),
+                StreetName = GetFunctions.GetStreetName(),
+                HouseNumber = GetFunctions.GetHouseNumber()
+            }
+        };
+
+        await context.AddAsync(student);
+        await context.SaveChangesAsync();
+    }
+}
